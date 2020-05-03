f39724bf0690ba71e8ce58cf32fcdda5e3c25687
Yuvaraj <yuva.ram11.com>
2020-05-03 21:15:55 +0530

    Updates

---
+++ precache-manifest.c914b412d4e6784b4fdf222c3184d2f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c597f26fbc0d7e6e90de99ae1a0c96d0",
+    "url": "/exercise-tracker/index.html"
+  },
+  {
+    "revision": "992b099795798b71ef12",
+    "url": "/exercise-tracker/static/css/2.392f69cd.chunk.css"
+  },
+  {
+    "revision": "992b099795798b71ef12",
+    "url": "/exercise-tracker/static/js/2.9aa229dc.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-tracker/static/js/2.9aa229dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793750e41580c1481711",
+    "url": "/exercise-tracker/static/js/main.dc80ebde.chunk.js"
+  },
+  {
+    "revision": "3c3ace677b4ef44724ee",
+    "url": "/exercise-tracker/static/js/runtime-main.30e212f4.js"
+  }
+]);
